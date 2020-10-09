4c5e7f497f1eec857fb4ce55e49c236f01675fb8
Antonio <antonio.axelsson@gmail.com>
2020-10-09 11:23:13 +0200

    Antonios tenta, 9 okt

--- 01.js
+++ 01.js
@@ -4,7 +4,33 @@
 - Spara alla värden i en array i en array som heter fizzbuzz.
 - Skriv ut arrayen.
 
-FizzBuzz är ett elementärt programmeringsproblem där man skriver ut tal i en talserie. Om det aktuella talet är jämnt delbart med 3 skriver man ut Fizz, är det jämnt delbart med 5 skriver man ut Buzz, är det jämnt delbart med båda skriver man ut FizzBuzz och annars skriver man ut talet. I detta fall skriver ni alltså dock inte ut värdet utan sparar det i arrayen och skriver ut arrayen när ni är klara.
+FizzBuzz är ett elementärt programmeringsproblem där man skriver
+ut tal i en talserie. Om det aktuella talet är jämnt delbart med 3 
+skriver man ut Fizz, är det jämnt delbart med 5 skriver man ut Buzz, 
+är det jämnt delbart med båda skriver man ut FizzBuzz och annars 
+skriver man ut talet. I detta fall skriver ni alltså dock inte ut 
+värdet utan sparar det i arrayen och skriver ut arrayen när ni är klara.
 
 */
 
+function fizzBuzz() {
+  let fizzBuzzArr = [];
+  for (let i = 0; i <= 100; i++) {
+    if (i === 0) {
+      fizzBuzzArr.push(i);
+    } else {
+      if (i % 3 === 0 && i % 5 === 0) {
+        fizzBuzzArr.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        fizzBuzzArr.push("Fizz");
+      } else if (i % 5 === 0) {
+        fizzBuzzArr.push("Buzz");
+      } else {
+        fizzBuzzArr.push(i);
+      }
+    }
+  }
+  return fizzBuzzArr;
+};
+
+console.log(fizzBuzz());
--- 02.js
+++ 02.js
@@ -1,24 +1,49 @@
 /*
 
-Skriv en funktion som tar en sträng som parameter, gör om alla tecken som inte är a-z (stora eller små) till HTML-entiteter och returnerar resultatet.
+Skriv en funktion som tar en sträng som parameter, 
+gör om alla tecken som inte är a-z (stora eller små) 
+till HTML-entiteter och returnerar resultatet.
 
 Läs mer om HTML-entiteter här: https://dev.w3.org/html5/html-author/charref
 
-Din funktion måste byta ut alla "osäkra" tecken mot "säkra". De tecken ni behöver byta ut är:
-<
->
-å
-ä
-ö
-'
+Din funktion måste byta ut alla "osäkra" tecken mot 
+"säkra". De tecken ni behöver byta ut är:
+//å = &aring;
+//ä = &auml;
+//ö = &ouml
+//< = &lt;
+//> = &gt;
+/// = &sol;
+//' = &apos;
 
-Alla tecken som inte är a-z eller någon av ovanstående (t ex kommatecken, punkter, bindestreck osv) ska tas bort.
+Alla tecken som inte är a-z eller någon av ovanstående 
+(t ex kommatecken, punkter, bindestreck osv) ska tas bort.
 
+
+
+function safe_string(myString) {
+  myString = myString.split("å").join("&aring;");
+  myString = myString.split("ä").join("&auml;");
+  myString = myString.split("ö").join("&ouml;");
+  myString = myString.split("<").join("&lt;");
+  myString = myString.split(">").join("&gt;");
+  myString = myString.split("/").join("&sol;");
+  myString = myString.split("'").join("&apos;");
+  return myString;
+}
 */
 
-function safe_string() {
-
+function safe_string(myString) {
+  myString = myString.split("å").join("&aring;")
+    .split("ä").join("&auml;")
+    .split("ö").join("&ouml;")
+    .split("<").join("&lt;")
+    .split(">").join("&gt;")
+    .split("/").join("&sol;")
+    .split("'").join("&apos;")
+    .replace(/[`~!@#$%^&*()_|+\-=?:'",.]/gi, '');
+    return myString;
 }
 
-console.log( safe_string( "<h1>Sjörövare, O'hoj</h1>" ) ); // Expected output: &lt;h1&gt;Sj&ouml;r&ouml;vare O&apos;hoj&lt;/h1&gt;
-
+console.log(safe_string("<h1>Sjörövare, O'hoj</h1>"));
+// Expected output: &lt;h1&gt;Sj&ouml;r&ouml;vare O&apos;hoj&lt;/h1&gt;
